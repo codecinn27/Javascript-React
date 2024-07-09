728fe9aeaeceb1b66333526c29bcb68f48318b14
Acer <codecinnpro@gmail.com>
2024-07-10 01:50:37 +0800

    update

---
+++ react/lv7_useeffect/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Counter from './counter';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Counter/>
+      <div>hihi</div>
+    </>
+  )
+}
+
+export default App
