e62e1896df452ef7515e64746b264f754afa00e0
codecinn27 <codecinnpro@gmail.com>
2024-07-15 00:02:35 +0800

    15/7

--- react/lv10_todo_mobile/src/TodoItem.jsx
+++ react/lv10_todo_mobile/src/TodoItem.jsx
@@ -5,7 +5,6 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 export default function TodoItem({todo, removeTodo, toggle}){
