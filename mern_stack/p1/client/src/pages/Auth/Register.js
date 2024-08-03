4c5bd95f31b3033bf364bc23cb5ee8c91c835951
codecinn27 <codecinnpro@gmail.com>
2024-08-04 00:16:40 +0800

    mern stack p1 add sign in icon and ui design

--- mern_stack/p1/client/src/pages/Auth/Register.js
+++ mern_stack/p1/client/src/pages/Auth/Register.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {FcGoogle} from "react-icons/fc";
 
 function Register() {
     const [user, setUser] = useState({email: "", password: ""});
+    const [showPassword, setShowPassword] = useState(false);
     const handleRegister = async(e)=>{
         e.preventDefault();
     }
@@ -25,6 +28,8 @@ function Register() {
         })
     }
 
+    const passwordCheck = (e)=>{ setShowPassword(e.target.checked)};
+
   return (
     <div className='flex items-center justify-center h-screen bg-gradient-to-r from-purple-400 to-red-500'>
         <form className='w-full max-w-sm bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4' onSubmit={handleRegister}>
@@ -36,14 +41,28 @@ function Register() {
             </div>
             <div className='mb-6'>
                 <label htmlFor='password' className='block text-grey-700 text-sm font-bold mb-2'>Password</label>
-                <input type="password" name='password' id="password" onChange={updatePassword}
+                <input type={showPassword? "text": "password"} name='password' id="password" onChange={updatePassword}
                 className='shadow border rounded w-full py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' />
+                <div className='flex items-center mt-2'>
+                    <input type='checkbox' checked={showPassword} onChange={passwordCheck} id='show-password'/>
+                    <label htmlFor='show-password' className='text-xs text-gray-600 ml-1'>Show Password</label>
+                </div>
             </div>
-            <div className='flex items-center justify-between'>
+            <div className='flex items-center justify-between mb-6'>
                 <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4
                 rounded focus:outline-none focus:shadow-outline' type='submit'>Submit</button>
+                <p className='text-sm text-red-500 hover:text-red-700'>Already registered ?{"  "}
+                    <Link to="/signin" className='font-bold'>Sign In</Link>
+                </p>
             </div>
-            
+            <div>
+                <Link to="http://localhost:5000/auth/google">
+                    <p className='flex items-center justify-center border border-gray-400 text-gray font-bold py-2 
+                    px-4 rounded w-full cursor-pointer'>
+                        <FcGoogle className='mr-2 h-6 w-6'/>Sign In with Google
+                    </p>
+                </Link>
+            </div>
         </form>
     </div>
   )
